c2e95251d3b8afff334db60680a5ab77b9047560
kaori kohama <kkaori131415@gmail.com>
2020-02-19 14:52:09 +0100

    created controller and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create!([
+  {name: "Febo", address: "Amsterdam", phone_number: "123-234-345", category: "chinese"},
+  {name: "Salsa shop", address: "Amsterdam", phone_number: "139-235-365", category: "japanese"},
+  {name: "Mcdonalds", address: "Tokyo", phone_number: "145-294-965", category: "belgian"},
+  {name: "Burger King", address: "Paris", phone_number: "126-237-345", category: "french"},
+  {name: "Burger boss", address: "Amsterdam", phone_number: "1259-236-099", category: "japanese"}
+])
